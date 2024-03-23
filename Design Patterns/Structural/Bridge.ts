581c76191dc314ad72e4c672738d0cc3de1d1e49
Ahmad Jafari <a99jafari@gmail.com>
2024-03-23 20:18:43 +0330

    Add examples of strcutural design patterns

---
+++ Design Patterns/Structural/Bridge.ts
@@ -0,0 +1,70 @@
+interface Player {
+  play(): string;
+  stop(): string;
+}
+
+class AudioPlayer implements Player {
+  play(): string {
+    return "Audio is playing";
+  }
+
+  stop(): string {
+    return "Audio is stopped";
+  }
+}
+
+class VideoPlayer implements Player {
+  play(): string {
+    return "Video is playing";
+  }
+
+  stop(): string {
+    return "Video is stopped";
+  }
+}
+
+interface Platform {
+  play(): string;
+  stop(): string;
+}
+
+class Desktop implements Platform {
+  private player: Player;
+
+  constructor(player: Player) {
+    this.player = player;
+  }
+
+  play(): string {
+    return this.player.play() + " on desktop";
+  }
+
+  stop(): string {
+    return this.player.stop() + " on desktop";
+  }
+}
+
+class Mobile implements Platform {
+  private player: Player;
+
+  constructor(player: Player) {
+    this.player = player;
+  }
+
+  play(): string {
+    return this.player.play() + " on mobile";
+  }
+
+  stop(): string {
+    return this.player.stop() + " on mobile";
+  }
+}
+
+// Usage
+const audioPlayer = new AudioPlayer();
+const videoPlayer = new VideoPlayer();
+
+const desktopVideoPlayer = new Desktop(videoPlayer);
+const desktopAudioPlayer = new Desktop(audioPlayer);
+const mobileVideoPlayer = new Mobile(videoPlayer);
+const mobileAudioPlayer = new Mobile(audioPlayer);
---
+++ Design Patterns/Structural/Flyweight (Cache).ts
@@ -0,0 +1,70 @@
+interface IRequest {
+  readonly method: "GET" | "POST" | "PUT" | "DELETE";
+  readonly url: string;
+  readonly body: Record<string, string>;
+  send(): Promise<any>;
+}
+
+class MinimalRequest implements IRequest {
+  constructor(
+    public readonly method: "GET" | "POST" | "PUT" | "DELETE",
+    public readonly url: string,
+    public readonly body: Record<string, string> = {}
+  ) {}
+
+  public async send(): Promise<any> {
+    const options = { method: this.method, body: JSON.stringify(this.body) };
+
+    const response = await fetch(this.url, options);
+
+    return response.json();
+  }
+}
+
+class RequestFactory {
+  private requests: Map<string, IRequest> = new Map();
+
+  public createRequest(
+    method: "GET" | "POST" | "PUT" | "DELETE",
+    url: string,
+    body: Record<string, string> = {}
+  ): IRequest {
+    const key = `${method}-${url}`;
+
+    if (!this.requests.has(key)) {
+      const request = new MinimalRequest(method, url, body);
+      this.requests.set(key, request);
+    }
+
+    return this.requests.get(key)!; // Type assertion for clarity
+  }
+}
+
+class ParallelRequestsHandler {
+  private factory: RequestFactory;
+
+  constructor(factory: RequestFactory) {
+    this.factory = factory;
+  }
+
+  public async sendAll(
+    requestsInfo: {
+      method: "GET" | "POST" | "PUT" | "DELETE";
+      url: string;
+      body?: Record<string, string>;
+    }[]
+  ): Promise<any[]> {
+    const requests = requestsInfo.map((requestInfo) =>
+      this.factory.createRequest(
+        requestInfo.method,
+        requestInfo.url,
+        requestInfo.body
+      )
+    );
+    const responses = await Promise.all(
+      requests.map((request) => request.send())
+    );
+
+    return responses;
+  }
+}
