7f1b97a47f75d2ab71a201ae816e0f28a0593f5f
Ahmad Jafari <a99jafari@gmail.com>
2024-04-01 00:20:27 +0330

    Create .gitignore file and update structure of foders

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
 const fs = require("node:fs");
 
-const INTRODUCTION_DOCS_PATH = "./1 - Introduction";
-const FUNDAMENTALS_DOCS_PATH = "./2 - Fundamentals";
-const SOLID_DOCS_PATH = "./3 - SOLID";
-const DESIGN_PATTERNS_DOCS_PATH = "./4 - Design Patterns";
-const OUTRO_DOCS_PATH = "./5 - Outro";
-
 const JOINER = "\n\n[`⬆ BACK TO TOP ⬆`](#table-of-contents)";
 
-function getFilesContent(path) {
-  const fileNames = fs.readdirSync(path);
+const DOCS_DIRECTORY = "./Documentations";
+const subDirectoriesOfDocs = fs.readdirSync(DOCS_DIRECTORY);
 
-  return fileNames.map((fileName) => {
-    const fileContent = fs.readFileSync(`${path}/${fileName}`, "utf-8");
+const contentOfDocs = subDirectoriesOfDocs.reduce((docs, subDirectory) => {
+  const fileNames = fs.readdirSync(`${DOCS_DIRECTORY}/${subDirectory}`);
+
+  const contents = fileNames.map((fileName) => {
+    const fileContent = fs.readFileSync(`${DOCS_DIRECTORY}/${subDirectory}/${fileName}`, "utf-8");
 
     return `${fileContent.trim()}${JOINER}`;
-  })
-}
+  });
 
-const INTRODUCTION_DOCS_CONTENTS = getFilesContent(INTRODUCTION_DOCS_PATH);
-const FUNDAMENTALS_DOCS_CONTENTS = getFilesContent(FUNDAMENTALS_DOCS_PATH);
-const SOLID_DOCS_CONTENTS = getFilesContent(SOLID_DOCS_PATH);
-const DESIGN_PATTERNS_DOCS_CONTENTS = getFilesContent(DESIGN_PATTERNS_DOCS_PATH);
-const OUTRO_DOCS_CONTENTS = getFilesContent(OUTRO_DOCS_PATH);
-
-const contentOfDocs = [
-  ...INTRODUCTION_DOCS_CONTENTS,
-  ...FUNDAMENTALS_DOCS_CONTENTS,
-  ...SOLID_DOCS_CONTENTS,
-  ...DESIGN_PATTERNS_DOCS_CONTENTS,
-  ...OUTRO_DOCS_CONTENTS,
-];
+  return [...docs, ...contents];
+}, []);
 
 const fullDocumentation = contentOfDocs.join("\n\n");
 
